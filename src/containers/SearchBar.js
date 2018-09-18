e8ea5a7d88c68b50b548addecc459aee5453ad8b
kamal11 <kyzx11@gmail.com>
2018-09-18 23:30:17 +0530

    Added state to Search Bar

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
+  state = {
+    text: ""
+  };
+
+  onChangeText = e => {
+    this.setState({ text: e.target.value });
+  };
+
   render() {
     return (
       <div>
         <form className="input-group">
-          <input type="text" />
+          <input
+            className="form-control"
+            placeholder="Get a 5-day forecast of your favorite cities"
+            value={this.state.text}
+            onChange={this.onChangeText}
+          />
           <span className="input-group-btn">
             <button type="submit" className="btn btn-secondary">
               Submit
