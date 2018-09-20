4c7eb6a333d4aa9eb82a4ccd9785fa905a251ae5
kamal11 <kyzx11@gmail.com>
2018-09-20 23:47:24 +0530

    Wired up Weather reducer

---
+++ src/reducers/WeatherReducer.js
@@ -0,0 +1,8 @@
+import { FETCH_WEATHER } from "../actions/index";
+export default (state = [], action) => {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      return [action.payload.data, ...state]; //this is for when user searches more city, so we are pushing them to state array
+  }
+  return state;
+};
