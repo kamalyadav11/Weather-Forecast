7d53d07f4c69d53882557897c53d51e2b678e5bb
kamal11 <kyzx11@gmail.com>
2018-09-20 22:00:41 +0530

    Added Api key and axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,14 @@
+//Middlewares are function that takes actions and depending on actions type and action's payload, the middleware can choose to let the action pass through, manipulate it, stop it, log it before it reaches reducer. Like gateKeeper to reducers.
+import axios from "axios";
 const API_KEY = "051213c218b6f19a74e50286f9635adf";
-//Middlewares are function that takes actions and depending on actions type and actions payload, the middleware can choose to let the action pass through,manipulate, stop it, log it before it reaches rteduicer. Like gateKeeper to reducers.
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`;
+
+const FETCH_WEATHER = "FETCH_WEATHER";
+
+export const fetchWeather = city => {
+  const url = `${ROOT_URL}&${city}`;
+  const request = axios.get(url);
+
+  type: FETCH_WEATHER;
+  payload: request;
+};
